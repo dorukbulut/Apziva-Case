23a84a8453275b4b5e5b3e64647f31d22dfbc7e5
Doruk Bulut <76884139+dorukbulut@users.noreply.github.com>
2022-09-25 22:48:39 +0300

    Update setting.py

--- ApzivaCase/settings.py
+++ ApzivaCase/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!ei#oyhoc#!=l!n)q4m)o1dq6z4wr+rs+s!*drkbdasf=odvc8'
+SECRET_KEY = 'YOUR DJANGO SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
